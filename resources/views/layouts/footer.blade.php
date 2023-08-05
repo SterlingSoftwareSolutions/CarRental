38890cee189a13742ff3fad8b1684f5873e8447f
DilshadMr360 <mohomeddilshad123@gmail.com>
2023-08-05 16:49:23 +0530

    this is about us page not register page
    Front-end done with mobile responsive, no custom css.

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,91 @@
+<div class="flex getoff-section z-50 ">
+    <div class="max-w-screen-lg mx-auto md:px-8 py-12 transition-all duration-500 ease-linear flex items-center">
+    <div>
+        <h1 class="text-white text-4xl font-bold inline-block">Get 15% Off Your Rental! Choose Your Model</h1>
+    </div>
+</div>
+<div class="max-w-screen-lg mx-auto px-4 md:px-8 py-12 transition-all duration-500 ease-linear flex items-center">
+    <div >
+        <button class="bg-white p-2 px-10 rounded mt-3 text-lg font-bold">Book Now</button>
+    </div>
+</div>
+</div>
+    <!-- end get off section -->
+<footer class="bg-emerald-600 dark:bg-gray-900 -mt-1">
+    <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+        <div class="md:flex md:justify-between">
+          <div class="mb-6 md:mb-0">
+              <a href="https://flowbite.com/" class="grid items-center">
+                  <p class="text-white">Lorem Ipsum Dolor Sit Amet, Consectetur <br> Adipiscing Elit. Sed Non Risus. Suspendisse <br> Lectus Tortor, Dignissim Sit Amet, </p>
+                  <div class="subscribe-form mt-5">
+                        <form action="#">
+                            <input class="bg-white" type="text" placeholder="Email Address">
+                            <button><img src="{{ URL('images/send.png')}}"> </button>
+                        </form>
+                  </div>
+              </a>
+          </div>
+          <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols">
+              <div>
+                  <h2 class="mb-6 text-lg font-semibold text-[#E0AA87] uppercase dark:text-white">Resources</h2>
+                  <ul class="text-white dark:text-gray-400 font-medium">
+                      <li class="mb-4">
+                          <a href="https://flowbite.com/" class="hover:underline">Home</a>
+                      </li>
+                      <li>
+                          <a href="https://tailwindcss.com/" class="hover:underline">Vehicle List</a>
+                      </li>
+                      <li>
+                          <a href="https://tailwindcss.com/" class="hover:underline">About Us</a>
+                      </li>
+                      <li>
+                          <a href="https://tailwindcss.com/" class="hover:underline">Contact Us</a>
+                      </li>
+                  </ul>
+              </div>
+              <div>
+                  <h2 class="mb-6 text-lg font-semibold text-[#E0AA87] uppercase dark:text-white">Follow us</h2>
+                  <ul class="text-white dark:text-gray-400 font-medium">
+                      <li class="mb-4 flex">
+                          <a href="" class="hover:underline flex "><img class="w-5 h-5" src="{{ URL('images/location.png')}}">&nbsp 5Th Floor, AH Building  Melbourne, Australia</a>
+                      </li>
+                      <li class="mb-4 flex">
+                          <a href="https://discord.gg/4eeurUVvTy" class="hover:underline flex"><img class="w-5 h-5" src="{{ URL('images/email.png')}}">&nbsp info@carrental.com</a>
+                      </li>
+                      <li class="mb-4 flex">
+                          <a href="https://discord.gg/4eeurUVvTy" class="hover:underline flex"><img class="w-5 h-5" src="{{ URL('images/telephone.png')}}">&nbsp + 8801 738 5678 64</a>
+                      </li>
+                  </ul>
+              </div>
+          </div>
+      </div>
+      <div class="flex items-center justify-center">
+    <div class="flex-1">
+        <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+    </div>
+    <div class="mx-3">
+        <a href="#" class="text-gray-400 hover:text-gray-500">
+            <i class="fab fa-facebook-f fa-lg"></i>
+        </a>
+    </div>
+    <div class="mx-3">
+        <a href="#" class="text-gray-400 hover:text-gray-500">
+            <i class="fab fa-instagram fa-lg"></i>
+        </a>
+    </div>
+    <div class="mx-3">
+        <a href="#" class="text-gray-400 hover:text-gray-500">
+            <i class="fab fa-twitter fa-lg"></i>
+        </a>
+    </div>
+    <div class="flex-1">
+        <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+    </div>
+</div>
+
+      <div class="text-sm text-white sm:text-center dark:text-gray-400 text-center sm:flex sm:justify-center">
+    <span>© Copyright 2023 <a href="https://flowbite.com/" class="hover:underline">by Automobex Car Rental</a></span>
+      </div>
+
+    </div>
+</footer>
