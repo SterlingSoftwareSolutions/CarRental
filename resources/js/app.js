55f875afe7a458616003b833d62c19da723497a1
Mhm Nimshan <madusanka.senadhi@towcha.com.au>
2023-08-07 06:15:20 +0530

    Change navigation

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,76 +1,132 @@
-import './bootstrap';
+import "./bootstrap";
 
-import Alpine from 'alpinejs';
+import Alpine from "alpinejs";
 
 window.Alpine = Alpine;
 
 Alpine.start();
+
+// car listng filtering drop down
+const dropdowns = document.querySelectorAll(".dropdown");
+
+dropdowns.forEach((dropdown) => {
+    const select = dropdown.querySelector(".select");
+    const caret = dropdown.querySelector(".caret");
+    const menu = dropdown.querySelector(".menu");
+    const options = dropdown.querySelectorAll(".menu li");
+    const selected = dropdown.querySelector(".selected");
+
+    select.addEventListener("click", () => {
+        select.classList.toggle("select-clicked");
+        caret.classList.toggle("caret-rotate");
+        menu.classList.toggle("menu-open");
+    });
+
+    options.forEach((option) => {
+        option.addEventListener("click", () => {
+            selected.innerText = option.innerText;
+            select.classList.remove("select-clicked");
+            caret.classList.remove("caret-rotate");
+            menu.classList.remove("menu-open");
+
+            options.forEach((option) => {
+                option.classList.remove("active");
+            });
+            option.classList.add("active");
+        });
+    });
+});
+
+// end car listing filtering drop down
+
+// nav js
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links li");
+
+hamburger.addEventListener("click", () => {
+    //Animate Links
+    navLinks.classList.toggle("open");
+    links.forEach((link) => {
+        link.classList.toggle("fade");
+    });
+
+    //Hamburger Animation
+    hamburger.classList.toggle("toggle");
+});
+
+    //Hamburger Animation
+    
+
+// end nav js
 
 
 // serach foam script
 const selectedAll = document.querySelectorAll(".wrapper-dropdown");
 
 selectedAll.forEach((selected) => {
-  const optionsContainer = selected.children[2];
-  const optionsList = selected.querySelectorAll("div.wrapper-dropdown li");
+    const optionsContainer = selected.children[2];
+    const optionsList = selected.querySelectorAll("div.wrapper-dropdown li");
 
-  selected.addEventListener("click", () => {
-    let arrow = selected.children[1];
+    selected.addEventListener("click", () => {
+        let arrow = selected.children[1];
 
-    if (selected.classList.contains("active")) {
-      handleDropdown(selected, arrow, false);
-    } else {
-      let currentActive = document.querySelector(".wrapper-dropdown.active");
+        if (selected.classList.contains("active")) {
+            handleDropdown(selected, arrow, false);
+        } else {
+            let currentActive = document.querySelector(
+                ".wrapper-dropdown.active"
+            );
 
-      if (currentActive) {
-        let anotherArrow = currentActive.children[1];
-        handleDropdown(currentActive, anotherArrow, false);
-      }
+            if (currentActive) {
+                let anotherArrow = currentActive.children[1];
+                handleDropdown(currentActive, anotherArrow, false);
+            }
 
-      handleDropdown(selected, arrow, true);
-    }
-  });
-
-  // update the display of the dropdown
-  for (let o of optionsList) {
-    o.addEventListener("click", () => {
-      selected.querySelector(".selected-display").innerHTML = o.innerHTML;
+            handleDropdown(selected, arrow, true);
+        }
     });
-  }
+
+    // update the display of the dropdown
+    for (let o of optionsList) {
+        o.addEventListener("click", () => {
+            selected.querySelector(".selected-display").innerHTML = o.innerHTML;
+        });
+    }
 });
 
 // check if anything else ofther than the dropdown is clicked
 window.addEventListener("click", function (e) {
-  if (e.target.closest(".wrapper-dropdown") === null) {
-    closeAllDropdowns();
-  }
+    if (e.target.closest(".wrapper-dropdown") === null) {
+        closeAllDropdowns();
+    }
 });
 
 // close all the dropdowns
 function closeAllDropdowns() {
-  const selectedAll = document.querySelectorAll(".wrapper-dropdown");
-  selectedAll.forEach((selected) => {
-    const optionsContainer = selected.children[2];
-    let arrow = selected.children[1];
+    const selectedAll = document.querySelectorAll(".wrapper-dropdown");
+    selectedAll.forEach((selected) => {
+        const optionsContainer = selected.children[2];
+        let arrow = selected.children[1];
 
-    handleDropdown(selected, arrow, false);
-  });
+        handleDropdown(selected, arrow, false);
+    });
 }
 
 // open all the dropdowns
 function handleDropdown(dropdown, arrow, open) {
-  if (open) {
-    arrow.classList.add("rotated");
-    dropdown.classList.add("active");
-  } else {
-    arrow.classList.remove("rotated");
-    dropdown.classList.remove("active");
-  }
+    if (open) {
+        arrow.classList.add("rotated");
+        dropdown.classList.add("active");
+    } else {
+        arrow.classList.remove("rotated");
+        dropdown.classList.remove("active");
+    }
 }
 
 // end serach foam script
 
-// drverse scrolle script 
+// drverse scrolle script
 
 const slidesContainer = document.querySelector(".slides-container");
 const slideWidth = slidesContainer.querySelector(".slide").clientWidth;
@@ -78,35 +134,35 @@ const prevButton = document.querySelector(".prev");
 const nextButton = document.querySelector(".next");
 
 nextButton.addEventListener("click", () => {
-	slidesContainer.scrollLeft += slideWidth;
+    slidesContainer.scrollLeft += slideWidth;
 });
 
 prevButton.addEventListener("click", () => {
-	slidesContainer.scrollLeft -= slideWidth;
+    slidesContainer.scrollLeft -= slideWidth;
 });
 
-// counter script 
+// counter script
 const counters = document.querySelectorAll(".count");
 const speed = 10;
 
 counters.forEach((counter) => {
-  const updateCount = () => {
-    const target = parseInt(+counter.getAttribute("data-target"));
-    const count = parseInt(+counter.innerText);
-    const increment = Math.trunc(target / speed);
-    console.log(increment);
+    const updateCount = () => {
+        const target = parseInt(+counter.getAttribute("data-target"));
+        const count = parseInt(+counter.innerText);
+        const increment = Math.trunc(target / speed);
+        console.log(increment);
 
-    if (count < target) {
-      counter.innerText = count + increment;
-      setTimeout(updateCount, 1);
-    } else {
-      count.innerText = target;
-    }
-  };
-  updateCount();
+        if (count < target) {
+            counter.innerText = count + increment;
+            setTimeout(updateCount, 1);
+        } else {
+            count.innerText = target;
+        }
+    };
+    updateCount();
 });
 
-// end counter script 
+// end counter script
 
 // testimonials script
 const reviewWrap = document.getElementById("reviewWrap");
@@ -122,41 +178,37 @@ const chicken = document.querySelector(".chicken");
 let isChickenVisible;
 
 let people = [
-	{
-		photo:
-			'url("https://cdn.pixabay.com/photo/2018/03/06/22/57/portrait-3204843_960_720.jpg")',
-		name: "Susan Smith",
-		profession: "WEB DEVELOPER",
-		description:
-			"Cheese and biscuits chalk and cheese fromage frais. Cheeseburger caerphilly cheese slices chalk and cheese cheeseburger mascarpone danish fontina rubber cheese. Squirty cheese say cheese manchego jarlsberg lancashire taleggio cheese and wine squirty cheese. Babybel pecorino feta macaroni cheese brie queso everyone loves gouda. Cheese and biscuits camembert de normandie fromage fromage macaroni cheese"
-	},
+    {
+        photo: 'url("https://cdn.pixabay.com/photo/2018/03/06/22/57/portrait-3204843_960_720.jpg")',
+        name: "Susan Smith",
+        profession: "WEB DEVELOPER",
+        description:
+            "Cheese and biscuits chalk and cheese fromage frais. Cheeseburger caerphilly cheese slices chalk and cheese cheeseburger mascarpone danish fontina rubber cheese. Squirty cheese say cheese manchego jarlsberg lancashire taleggio cheese and wine squirty cheese. Babybel pecorino feta macaroni cheese brie queso everyone loves gouda. Cheese and biscuits camembert de normandie fromage fromage macaroni cheese",
+    },
 
-	{
-		photo:
-			"url('https://cdn.pixabay.com/photo/2019/02/11/20/20/woman-3990680_960_720.jpg')",
-		name: "Anna Grey",
-		profession: "UFC FIGHTER",
-		description:
-			"I'm baby migas cornhole hell of etsy tofu, pickled af cardigan pabst. Man braid deep v pour-over, blue bottle art party thundercats vape. Yr waistcoat whatever yuccie, farm-to-table next level PBR&B. Banh mi pinterest palo santo, aesthetic chambray leggings activated charcoal cred hammock kitsch humblebrag typewriter neutra knausgaard. Pabst succulents lo-fi microdosing portland gastropub Banh mi pinterest palo santo"
-	},
+    {
+        photo: "url('https://cdn.pixabay.com/photo/2019/02/11/20/20/woman-3990680_960_720.jpg')",
+        name: "Anna Grey",
+        profession: "UFC FIGHTER",
+        description:
+            "I'm baby migas cornhole hell of etsy tofu, pickled af cardigan pabst. Man braid deep v pour-over, blue bottle art party thundercats vape. Yr waistcoat whatever yuccie, farm-to-table next level PBR&B. Banh mi pinterest palo santo, aesthetic chambray leggings activated charcoal cred hammock kitsch humblebrag typewriter neutra knausgaard. Pabst succulents lo-fi microdosing portland gastropub Banh mi pinterest palo santo",
+    },
 
-	{
-		photo:
-			"url('https://cdn.pixabay.com/photo/2016/11/21/12/42/beard-1845166_960_720.jpg')",
-		name: "Branson Cook",
-		profession: "ACTOR",
-		description:
-			"Radio telescope something incredible is waiting to be known billions upon billions Jean-François Champollion hearts of the stars tingling of the spine. Encyclopaedia galactica not a sunrise but a galaxyrise concept of the number one encyclopaedia galactica from which we spring bits of moving fluff. Vastness is bearable only through love paroxysm of global death concept"
-	},
+    {
+        photo: "url('https://cdn.pixabay.com/photo/2016/11/21/12/42/beard-1845166_960_720.jpg')",
+        name: "Branson Cook",
+        profession: "ACTOR",
+        description:
+            "Radio telescope something incredible is waiting to be known billions upon billions Jean-François Champollion hearts of the stars tingling of the spine. Encyclopaedia galactica not a sunrise but a galaxyrise concept of the number one encyclopaedia galactica from which we spring bits of moving fluff. Vastness is bearable only through love paroxysm of global death concept",
+    },
 
-	{
-		photo:
-			"url('https://cdn.pixabay.com/photo/2014/10/30/17/32/boy-509488_960_720.jpg')",
-		name: "Julius Grohn",
-		profession: "PROFESSIONAL CHILD",
-		description:
-			"Biscuit chocolate pastry topping lollipop pie. Sugar plum brownie halvah dessert tiramisu tiramisu gummi bears icing cookie. Gummies gummi bears pie apple pie sugar plum jujubes. Oat cake croissant bear claw tootsie roll caramels. Powder ice cream caramels candy tiramisu shortbread macaroon chocolate bar. Sugar plum jelly-o chocolate dragée tart chocolate marzipan cupcake gingerbread."
-	}
+    {
+        photo: "url('https://cdn.pixabay.com/photo/2014/10/30/17/32/boy-509488_960_720.jpg')",
+        name: "Julius Grohn",
+        profession: "PROFESSIONAL CHILD",
+        description:
+            "Biscuit chocolate pastry topping lollipop pie. Sugar plum brownie halvah dessert tiramisu tiramisu gummi bears icing cookie. Gummies gummi bears pie apple pie sugar plum jujubes. Oat cake croissant bear claw tootsie roll caramels. Powder ice cream caramels candy tiramisu shortbread macaroon chocolate bar. Sugar plum jelly-o chocolate dragée tart chocolate marzipan cupcake gingerbread.",
+    },
 ];
 
 imgDiv.style.backgroundImage = people[0].photo;
@@ -167,109 +219,106 @@ let currentPerson = 0;
 
 //Select the side where you want to slide
 function slide(whichSide, personNumber) {
-	let reviewWrapWidth = reviewWrap.offsetWidth + "px";
-	let descriptionHeight = description.offsetHeight + "px";
-	//(+ or -)
-	let side1symbol = whichSide === "left" ? "" : "-";
-	let side2symbol = whichSide === "left" ? "-" : "";
+    let reviewWrapWidth = reviewWrap.offsetWidth + "px";
+    let descriptionHeight = description.offsetHeight + "px";
+    //(+ or -)
+    let side1symbol = whichSide === "left" ? "" : "-";
+    let side2symbol = whichSide === "left" ? "-" : "";
 
-	let tl = gsap.timeline();
+    let tl = gsap.timeline();
 
-	if (isChickenVisible) {
-		tl.to(chicken, {
-			duration: 0.4,
-			opacity: 0
-		});
-	}
+    if (isChickenVisible) {
+        tl.to(chicken, {
+            duration: 0.4,
+            opacity: 0,
+        });
+    }
 
-	tl.to(reviewWrap, {
-		duration: 0.4,
-		opacity: 0,
-		translateX: `${side1symbol + reviewWrapWidth}`
-	});
+    tl.to(reviewWrap, {
+        duration: 0.4,
+        opacity: 0,
+        translateX: `${side1symbol + reviewWrapWidth}`,
+    });
 
-	tl.to(reviewWrap, {
-		duration: 0,
-		translateX: `${side2symbol + reviewWrapWidth}`
-	});
+    tl.to(reviewWrap, {
+        duration: 0,
+        translateX: `${side2symbol + reviewWrapWidth}`,
+    });
 
-	setTimeout(() => {
-		imgDiv.style.backgroundImage = people[personNumber].photo;
-	}, 400);
-	setTimeout(() => {
-		description.style.height = descriptionHeight;
-	}, 400);
-	setTimeout(() => {
-		personName.innerText = people[personNumber].name;
-	}, 400);
-	setTimeout(() => {
-		profession.innerText = people[personNumber].profession;
-	}, 400);
-	setTimeout(() => {
-		description.innerText = people[personNumber].description;
-	}, 400);
+    setTimeout(() => {
+        imgDiv.style.backgroundImage = people[personNumber].photo;
+    }, 400);
+    setTimeout(() => {
+        description.style.height = descriptionHeight;
+    }, 400);
+    setTimeout(() => {
+        personName.innerText = people[personNumber].name;
+    }, 400);
+    setTimeout(() => {
+        profession.innerText = people[personNumber].profession;
+    }, 400);
+    setTimeout(() => {
+        description.innerText = people[personNumber].description;
+    }, 400);
 
-	tl.to(reviewWrap, {
-		duration: 0.4,
-		opacity: 1,
-		translateX: 0
-	});
+    tl.to(reviewWrap, {
+        duration: 0.4,
+        opacity: 1,
+        translateX: 0,
+    });
 
-	if (isChickenVisible) {
-		tl.to(chicken, {
-			duration: 0.4,
-			opacity: 1
-		});
-	}
+    if (isChickenVisible) {
+        tl.to(chicken, {
+            duration: 0.4,
+            opacity: 1,
+        });
+    }
 }
 
 function setNextCardLeft() {
-	if (currentPerson === 3) {
-		currentPerson = 0;
-		slide("left", currentPerson);
-	} else {
-		currentPerson++;
-	}
+    if (currentPerson === 3) {
+        currentPerson = 0;
+        slide("left", currentPerson);
+    } else {
+        currentPerson++;
+    }
 
-	slide("left", currentPerson);
+    slide("left", currentPerson);
 }
 
 function setNextCardRight() {
-	if (currentPerson === 0) {
-		currentPerson = 3;
-		slide("right", currentPerson);
-	} else {
-		currentPerson--;
-	}
+    if (currentPerson === 0) {
+        currentPerson = 3;
+        slide("right", currentPerson);
+    } else {
+        currentPerson--;
+    }
 
-	slide("right", currentPerson);
+    slide("right", currentPerson);
 }
 
 leftArrow.addEventListener("click", setNextCardLeft);
 rightArrow.addEventListener("click", setNextCardRight);
 
 surpriseMeBtn.addEventListener("click", () => {
-	if (chicken.style.opacity === "0") {
-		chicken.style.opacity = "1";
-		imgDiv.classList.add("move-head");
-		surpriseMeBtn.innerText = "Remove the chicken";
-		surpriseMeBtn.classList.remove("surprise-me-btn");
-		surpriseMeBtn.classList.add("hide-chicken-btn");
-		isChickenVisible = true;
-	} else if (chicken.style.opacity === "1") {
-		chicken.style.opacity = "0";
-		imgDiv.classList.remove("move-head");
-		surpriseMeBtn.innerText = "Surprise me";
-		surpriseMeBtn.classList.add("surprise-me-btn");
-		surpriseMeBtn.classList.remove("hide-chicken-btn");
-		isChickenVisible = false;
-	}
+    if (chicken.style.opacity === "0") {
+        chicken.style.opacity = "1";
+        imgDiv.classList.add("move-head");
+        surpriseMeBtn.innerText = "Remove the chicken";
+        surpriseMeBtn.classList.remove("surprise-me-btn");
+        surpriseMeBtn.classList.add("hide-chicken-btn");
+        isChickenVisible = true;
+    } else if (chicken.style.opacity === "1") {
+        chicken.style.opacity = "0";
+        imgDiv.classList.remove("move-head");
+        surpriseMeBtn.innerText = "Surprise me";
+        surpriseMeBtn.classList.add("surprise-me-btn");
+        surpriseMeBtn.classList.remove("hide-chicken-btn");
+        isChickenVisible = false;
+    }
 });
 
 window.addEventListener("resize", () => {
-	description.style.height = "100%";
+    description.style.height = "100%";
 });
-
-
-
 // end testimonials script
