d759eecdfee623b7348381d594db884e52c8839a
Mhm Nimshan <madusanka.senadhi@towcha.com.au>
2023-08-07 09:26:06 +0530

    Update the car listing UI and style

--- resources/views/pages/carlist.blade.php
+++ resources/views/pages/carlist.blade.php
@@ -18,7 +18,7 @@
 
     <!-- banner section -->
     <div class="relative -z-30">
-        <img src="{{ URL('images/Group 148.png')}}" alt="" srcset="">
+        <img src="{{ URL('images/Group 180.png')}}" alt="" srcset="">
         <h1 class="absolute top-60 left-60 text-white text-4xl font-bold inline-block" text-4xl">Find Your Dream Ride</h1>
     </div>
     <!-- end banner section -->
@@ -83,7 +83,160 @@
     <!-- end filtering section -->
 
     <!-- car list -->
-    
+    <div class="grid justify-center items-center p-4">
+        <div class="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
+            <!-- Repeat this section for each car item -->
+            <div class="bg-[#F8FFF2] grid p-4">
+                <img class="rounded w-full h-56" src="{{ URL('images/Group 159.png')}}">
+                <div class="flex justify-between items-center p-4">
+                    <div class="flex w-4/6">
+                        <div class="bg-[#F8FFF2] grid">
+                            <h1>Toyota Crown</h1>
+                            <div class="flex items-center space-x-1">
+                                <svg class="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                                </svg>
+                                <svg class="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                                </svg>
+                                <svg class="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                                </svg>
+                                <svg class="w-4 h-4 text-gray-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                                </svg>
+                                <svg class="w-4 h-4 text-gray-300 dark:text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                                </svg>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="bg-emerald-600 px-1 rounded">
+                        <h1 class="text-white">$ 180 /hour</h1>
+                    </div>
+                </div>
+                <div class="flex justify-between items-center p-4">
+                    <div class="grid w-6/6 text-center">
+                        <img src="{{ URL('images/seat-belt.png') }}" class="mx-auto">
+                        <h1 class="text-center text-[#317256] font-semibold mt-2">4 Passengers</h1>
+                    </div>
+
+                    <div class="grid w-6/6 text-center">
+                        <img src="{{ URL('images/luggage.png')}}" class="mx-auto">
+                        <h1 class="text-center text-[#317256] font-semibold mt-2">5 Luggages</h1>
+                    </div>
+                    <div class="grid w-6/6 text-center">
+                        <img src="{{ URL('images/manual-transmission.png')}}" class="mx-auto">
+                        <h1 class="text-center text-[#317256] font-semibold mt-2">Auto</h1>
+                    </div>
+                </div>
+                <div class="flex justify-center items-center p-4">
+                    <button class="text-white bg-emerald-600 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 hover:text-white rounded text-lg">Book Now</button>
+                </div>
+            </div>
+            <div class="bg-[#F8FFF2] grid p-4">
+                <img class="rounded w-full h-56" src="{{ URL('images/Group 159.png')}}">
+                <div class="flex justify-between items-center p-4">
+                    <div class="flex w-4/6">
+                        <div class="bg-[#F8FFF2] grid">
+                            <h1>Toyota Crown</h1>
+                            <div class="flex items-center space-x-1">
+                                <svg class="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                                </svg>
+                                <svg class="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                                </svg>
+                                <svg class="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                                </svg>
+                                <svg class="w-4 h-4 text-gray-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                                </svg>
+                                <svg class="w-4 h-4 text-gray-300 dark:text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                                </svg>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="bg-emerald-600 px-1 rounded">
+                        <h1 class="text-white">$ 180 /hour</h1>
+                    </div>
+                </div>
+                <div class="flex justify-between items-center p-4">
+                    <div class="grid w-6/6 text-center">
+                        <img src="{{ URL('images/seat-belt.png') }}" class="mx-auto">
+                        <h1 class="text-center text-[#317256] font-semibold mt-2">4 Passengers</h1>
+                    </div>
+
+                    <div class="grid w-6/6 text-center">
+                        <img src="{{ URL('images/luggage.png')}}" class="mx-auto">
+                        <h1 class="text-center text-[#317256] font-semibold mt-2">5 Luggages</h1>
+                    </div>
+                    <div class="grid w-6/6 text-center">
+                        <img src="{{ URL('images/manual-transmission.png')}}" class="mx-auto">
+                        <h1 class="text-center text-[#317256] font-semibold mt-2">Auto</h1>
+                    </div>
+                </div>
+                <div class="flex justify-center items-center p-4">
+                    <button class="text-white bg-emerald-600 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 hover:text-white rounded text-lg">Book Now</button>
+                </div>
+            </div>
+            <div class="bg-[#F8FFF2] grid p-4">
+                <img class="rounded w-full h-56" src="{{ URL('images/Group 159.png')}}">
+                <div class="flex justify-between items-center p-4">
+                    <div class="flex w-4/6">
+                        <div class="bg-[#F8FFF2] grid">
+                            <h1>Toyota Crown</h1>
+                            <div class="flex items-center space-x-1">
+                                <svg class="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                                </svg>
+                                <svg class="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                                </svg>
+                                <svg class="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                                </svg>
+                                <svg class="w-4 h-4 text-gray-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                                </svg>
+                                <svg class="w-4 h-4 text-gray-300 dark:text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                                </svg>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="bg-emerald-600 px-1 rounded">
+                        <h1 class="text-white">$ 180 /hour</h1>
+                    </div>
+                </div>
+                <div class="flex justify-between items-center p-4">
+                    <div class="grid w-6/6 text-center">
+                        <img src="{{ URL('images/seat-belt.png') }}" class="mx-auto">
+                        <h1 class="text-center text-[#317256] font-semibold mt-2">4 Passengers</h1>
+                    </div>
+
+                    <div class="grid w-6/6 text-center">
+                        <img src="{{ URL('images/luggage.png')}}" class="mx-auto">
+                        <h1 class="text-center text-[#317256] font-semibold mt-2">5 Luggages</h1>
+                    </div>
+                    <div class="grid w-6/6 text-center">
+                        <img src="{{ URL('images/manual-transmission.png')}}" class="mx-auto">
+                        <h1 class="text-center text-[#317256] font-semibold mt-2">Auto</h1>
+                    </div>
+                </div>
+                <div class="flex justify-center items-center p-4">
+                    <button class="text-white bg-emerald-600 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 hover:text-white rounded text-lg">Book Now</button>
+                </div>
+            </div>
+            <!-- End of repeating section -->
+        </div>
+    </div>
+
+
+
+
     <!-- end car list -->
 
 
