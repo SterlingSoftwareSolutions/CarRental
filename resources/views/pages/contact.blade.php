7336190af6969efe4024e72c476a4830fde89169
Mhm Nimshan <madusanka.senadhi@towcha.com.au>
2023-08-09 11:29:26 +0530

    Update payment page

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -1,5 +1,6 @@
     <!doctype html>
     <html>
+
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,8 +8,9 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.7.1/gsap.min.js"></script>
         @vite('resources/css/app.css')
         @vite('resources/js/app.js')
-        
+
     </head>
+
     <body>
         <!-- start navigation -->
         @include('layouts.navigation')
@@ -25,33 +27,31 @@
             <h1 class="text-5xl font-bold text-emerald-600">Feel Free to Contact Us</h1>
         </div>
         <div class="relative w-5/6 mx-auto mt-10 px-4 sm:px-8 -z-40">
-        <div class="overflow-hidden rounded-lg shadow-md">
-            <iframe class="w-full h-64 sm:h-96"
-                    src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12080.73732861526!2d-74.0059418!3d40.7127847!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zM40zMDA2JzEwLjAiTiA3NMKwMjUnMzcuNyJX!5e0!3m2!1sen!2sus!4v1648482801994!5m2!1sen!2sus"
-                    frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0">
-            </iframe>
+            <div class="overflow-hidden rounded-lg shadow-md">
+                <iframe class="w-full h-64 sm:h-96" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12080.73732861526!2d-74.0059418!3d40.7127847!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zM40zMDA2JzEwLjAiTiA3NMKwMjUnMzcuNyJX!5e0!3m2!1sen!2sus!4v1648482801994!5m2!1sen!2sus" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0">
+                </iframe>
+            </div>
         </div>
-    </div>
         <!-- end map section -->
-        
+
         <!-- contact infomation section -->
         <div class="flex flex-col sm:flex-row w-4/6 mx-auto gap-4 sm:gap-8 p-4 sm:p-0 -mt-20 z-50">
-        <div class="bg-emerald-600 p-4 rounded-lg text-center flex-1">
-            <a href="#" class="hover:underline inline-flex items-center justify-center"><img src="{{ URL('images/bigloc.png')}}" class="w-12 h-12 sm:w-16 sm:h-16" alt="Location"></a>
-            <h1 class="text-white font-semibold text-base sm:text-lg">Our Location</h1>
-            <a href="#" class="text-white hover:underline block mt-2">5th Floor, AH Building, Melbourne, Australia</a>
+            <div class="bg-emerald-600 p-4 rounded-lg text-center flex-1">
+                <a href="#" class="hover:underline inline-flex items-center justify-center"><img src="{{ URL('images/bigloc.png')}}" class="w-12 h-12 sm:w-16 sm:h-16" alt="Location"></a>
+                <h1 class="text-white font-semibold text-base sm:text-lg">Our Location</h1>
+                <a href="#" class="text-white hover:underline block mt-2">5th Floor, AH Building, Melbourne, Australia</a>
+            </div>
+            <div class="bg-emerald-600 p-4 rounded-lg text-center flex-1">
+                <a href="#" class="hover:underline inline-flex items-center justify-center"><img src="{{ URL('images/bigmail.png')}}" class="w-12 h-12 sm:w-16 sm:h-16" alt="Email"></a>
+                <h1 class="text-white font-semibold text-base sm:text-lg">Email</h1>
+                <a href="#" class="text-white hover:underline block mt-2">Info@Carrental.Com</a>
+            </div>
+            <div class="bg-emerald-600 p-4 rounded-lg text-center flex-1">
+                <a href="#" class="hover:underline inline-flex items-center justify-center"><img src="{{ URL('images/bigsell.png')}}" class="w-12 h-12 sm:w-16 sm:h-16" alt="Phone"></a>
+                <h1 class="text-white font-semibold text-base sm:text-lg">Phone</h1>
+                <a href="#" class="text-white hover:underline block mt-2">+8801 738 5678 64</a>
+            </div>
         </div>
-        <div class="bg-emerald-600 p-4 rounded-lg text-center flex-1">
-            <a href="#" class="hover:underline inline-flex items-center justify-center"><img src="{{ URL('images/bigmail.png')}}" class="w-12 h-12 sm:w-16 sm:h-16" alt="Email"></a>
-            <h1 class="text-white font-semibold text-base sm:text-lg">Email</h1>
-            <a href="#" class="text-white hover:underline block mt-2">Info@Carrental.Com</a>
-        </div>
-        <div class="bg-emerald-600 p-4 rounded-lg text-center flex-1">
-            <a href="#" class="hover:underline inline-flex items-center justify-center"><img src="{{ URL('images/bigsell.png')}}" class="w-12 h-12 sm:w-16 sm:h-16" alt="Phone"></a>
-            <h1 class="text-white font-semibold text-base sm:text-lg">Phone</h1>
-            <a href="#" class="text-white hover:underline block mt-2">+8801 738 5678 64</a>
-        </div>
-    </div>
         <!-- end contact infomation section -->
 
         <!-- send message section -->
@@ -67,24 +67,24 @@
                 </a>
                 <div class="items-center md:flex-row md:max-w-xl dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
                     <div class="grid py-3.5 pl-5">
-                    <h1 class="text-white font-semibold text-3xl mb-4">Send us a Message</h1>
-                    <div class="container mx-auto flex">
-        <div class="rounded-lg flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10">
-        <div class="relative mb-4">
-            <input type="text" placeholder="Your Name *" id="name" name="name" class="w-full placeholder-emerald-600 bg-white border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
-        </div>
-        <div class="relative mb-4">
-            <input type="number" placeholder="Your Phone *" id="phone" name="phone" class="w-full placeholder-emerald-600 bg-white border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
-        </div>
-        <div class="relative mb-4">
-            <input type="email" placeholder="Your Email *" id="email" name="email" class="w-full placeholder-emerald-600 bg-white border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
-        </div>
-        <div class="relative mb-4">
-            <textarea id="message" placeholder="Message" name="message" class="w-full bg-white placeholder-emerald-600 border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
-        </div>
-        <button class="text-emerald-600 bg-white border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 hover:text-white rounded text-lg">Submit</button>
-        </div>
-    </div>
+                        <h1 class="text-white font-semibold text-3xl mb-4">Send us a Message</h1>
+                        <div class="container mx-auto flex">
+                            <div class="rounded-lg flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10">
+                                <div class="relative mb-4">
+                                    <input type="text" placeholder="Your Name *" id="name" name="name" class="w-full placeholder-emerald-600 bg-white border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                </div>
+                                <div class="relative mb-4">
+                                    <input type="number" placeholder="Your Phone *" id="phone" name="phone" class="w-full placeholder-emerald-600 bg-white border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                </div>
+                                <div class="relative mb-4">
+                                    <input type="email" placeholder="Your Email *" id="email" name="email" class="w-full placeholder-emerald-600 bg-white border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                </div>
+                                <div class="relative mb-4">
+                                    <textarea id="message" placeholder="Message" name="message" class="w-full bg-white placeholder-emerald-600 border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+                                </div>
+                                <button class="text-emerald-600 bg-white border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 hover:text-white rounded text-lg">Submit</button>
+                            </div>
+                        </div>
                     </div>
 
                 </div>
@@ -92,8 +92,11 @@
         </div>
         <!-- end send message section -->
         <!-- start navigation -->
-        @include('layouts.footer')
+        <div class="-mt-30">
+            @include('layouts.footer')
+        </div>
         <!-- end navigation -->
 
     </body>
+
     </html>
---
+++ resources/views/pages/payment.blade.php
@@ -0,0 +1,210 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.7.1/gsap.min.js"></script>
+    @vite('resources/css/app.css')
+    @vite('resources/js/app.js')
+
+</head>
+
+<body>
+    <!-- start navigation -->
+    @include('layouts.navigation')
+    <!-- end navigation -->
+    <!-- section one -->
+    <div class="flex md:flex-wrap md:justify-center md:items-start">
+        <div class=" p-4 md:w-full lg:w-4/12 grid grid-flow-row mt-24 sm:mt-18">
+            <div class="bg-[#F8FFF2] p-8">
+                <!-- car image -->
+                <div class="w-full">
+                    <img class="w-full" src="{{ URL('images/Rectangle 148.png') }}">
+                </div>
+                <!-- end car image -->
+                <!-- pickup details -->
+                <table class="w-full table-fixed mt-5">
+                    <tbody>
+                        <tr>
+                            <td class="border-b-2 border-dotted p-2">Title</td>
+                            <td class="border-b-2 border-dotted text-right p-2">2008 Toyota Crown</td>
+                        </tr>
+                        <tr>
+                            <td class="border-b-2 border-dotted p-2">Pick-up Date & Time</td>
+                            <td class="border-b-2 border-dotted text-right p-2">10/08/2023 At 7.30</td>
+                        </tr>
+                        <tr>
+                            <td class="border-b-2 border-dotted p-2">Drop-off Date & Time</td>
+                            <td class="border-b-2 border-dotted text-right p-2">11/08/2023 At 10.00</td>
+                        </tr>
+                        <tr>
+                            <td class="border-b-2 border-dotted p-2">Total Days</td>
+                            <td class="border-b-2 border-dotted text-right p-2">1 Day 3 Hours</td>
+                        </tr>
+                        <tr>
+                            <td class="border-b-2 border-dotted p-2">Pick-up Location</td>
+                            <td class="border-b-2 border-dotted text-right p-2">Sydney Airport (Metro), SYD</td>
+                        </tr>
+                        <tr>
+                            <td class="p-2">Drop-off Location</td>
+                            <td class="text-right p-2">MCG, Melbourne, Victoria</td>
+                        </tr>
+                    </tbody>
+                </table>
+                <!-- end pickup details -->
+            </div>
+            <div class="border-2 border-[#398564] p-8 rounded-xl mt-6">
+                <h1 class="text-center text-[#398564] font-semibold text-lg">Order Summary</h1>
+                <!-- order summery -->
+                <table class="w-full table-fixed mt-5">
+                    <tbody>
+                        <tr>
+                            <td class="border-b-2 border-dotted p-2">SUBTOTAL</td>
+                            <td class="border-b-2 border-dotted text-right p-2">AUD 168.00</td>
+                        </tr>
+                        <tr>
+                            <td class="border-b-2 border-dotted p-2">FINES</td>
+                            <td class="border-b-2 border-dotted text-right p-2">AUD 0.00</td>
+                        </tr>
+                        <tr>
+                            <td class="border-b-2 border-dotted p-2">INSURANCE</td>
+                            <td class="border-b-2 border-dotted text-right p-2">AUD 100.00</td>
+                        </tr>
+                        <tr>
+                            <td class="p-2 mt-2 font-bold">TOTAL</td>
+                            <td class="text-right p-2 font-bold">AUD 268.00</td>
+                        </tr>
+                    </tbody>
+                </table>
+                <!-- end order summery -->
+            </div>
+        </div>
+        <div class="md:w-full lg:w-4/12 grid grid-flow-row h-screen mt-24 sm:mt-18">
+            <!-- payment foam -->
+            <div class="bg-[#F8FFF2] grid md:p-8 md:mt-0">
+                <h1 class="text-[#707070] text-base">Let's get your personal account set up.</h1>
+                <hr>
+                <div class>
+                    <form action=" /action_page.php">
+                        <h1 class="text-black text-sm">Country <span class="text-red-500">*</span></h1>
+                        <select class="w-3/6 rounded-md border-none shadow-md" name="cars" id="cars">
+                            <option value="volvo">Volvo</option>
+                            <option value="saab">Saab</option>
+                            <option value="opel">Opel</option>
+                            <option value="audi">Audi</option>
+                        </select>
+                </div>
+                <h1 class="text-[#707070] text-base mt-6">Your Personal Information</h1>
+                <hr>
+                <div class="flex w-full gap-4 mt-5">
+                    <div class="flex-1">
+                        <h1 class="text-black text-sm">First Name <span class="text-red-500">*</span></h1>
+                        <input class="w-full rounded-md border-none shadow-md" type="text" name="" id="">
+                    </div>
+                    <div class="flex-1">
+                        <h1 class="text-black text-sm">Last Name <span class="text-red-500">*</span></h1>
+                        <input class="w-full rounded-md border-none shadow-md" type="text" name="" id="">
+                    </div>
+                </div>
+                <div class="flex w-full gap-4">
+                    <div class="flex-1">
+                        <h1 class="text-black text-sm">Mobile Number <span class="text-red-500">*</span></h1>
+                        <input class="w-full rounded-md border-none shadow-md" type="text" name="" id="">
+                    </div>
+                    <div class="flex-1">
+                        <h1 class="text-black text-sm">Email <span class="text-red-500">*</span></h1>
+                        <input class="w-full rounded-md border-none shadow-md" type="text" name="" id="">
+                    </div>
+                </div>
+                <div class="flex w-full gap-4">
+                    <div class="flex-1">
+                        <h1 class="text-black text-sm">Address 1 <span class="text-red-500">*</span></h1>
+                        <input class="w-full rounded-md border-none shadow-md" type="text" name="" id="">
+                    </div>
+                    <div class="flex-1">
+                        <h1 class="text-black text-sm">Address 2 <span class="text-red-500">*</span></h1>
+                        <input class="w-full rounded-md border-none shadow-md" type="text" name="" id="">
+                    </div>
+                </div>
+                <div class="flex w-full gap-4">
+                    <div class="flex-1">
+                        <h1 class="text-black text-sm">City <span class="text-red-500">*</span></h1>
+                        <input class="w-full rounded-md border-none shadow-md" type="text" name="" id="">
+                    </div>
+                    <div class="flex-1">
+                        <h1 class="text-black text-sm">Zip/Postal Code <span class="text-red-500">*</span></h1>
+                        <input class="w-full rounded-md border-none shadow-md" type="text" name="" id="">
+                    </div>
+                </div>
+                <h1 class="text-[#707070] text-base mt-8">Driver's License Information</h1>
+                <p class="text-[#707070] text-sm">We request this information so on your first rental you can skip the airport counter and pick your car up directly at the lot.</p>
+                <hr class="">
+                <div class="flex w-full gap-4 mt-5">
+                    <div class="flex-1">
+                        <h1 class="text-black text-sm">Driver's License Number <span class="text-red-500">*</span></h1>
+                        <input class="w-full rounded-md border-none shadow-md" type="text" name="" id="">
+                    </div>
+                    <div class="flex-1">
+                        <h1 class="text-black text-sm">Expired Year (YYYY) <span class="text-red-500">*</span></h1>
+                        <input class="w-full rounded-md border-none shadow-md" type="text" name="" id="">
+                    </div>
+                </div>
+                <div class="flex w-full gap-4">
+                    <div class="flex-1">
+                        <h1 class="text-black text-sm">Expired Month <span class="text-red-500">*</span></h1>
+                        <input class="w-full rounded-md border-none shadow-md" type="text" name="" id="">
+                    </div>
+                    <div class="flex-1">
+                        <h1 class="text-black text-sm">Expired Date (DD) <span class="text-red-500">*</span></h1>
+                        <input class="w-full rounded-md border-none shadow-md" type="text" name="" id="">
+                    </div>
+                </div>
+                <h1 class="text-[#707070] text-base mt-8">Credit Card Information</h1>
+                <p class="text-[#707070] text-sm">A credit card is required to keep on file for future reservations, no charge for enrolling. Debit Cards can NOT be used online for enrollment or for card updates for Automobex car Rental.</p>
+                <hr class="">
+                <div class="flex w-full gap-4 mt-5">
+                    <div class="flex-1">
+                        <h1 class="text-black text-sm">Driver's License Number <span class="text-red-500">*</span></h1>
+                        <input class="w-full rounded-md border-none shadow-md" type="text" name="" id="">
+                    </div>
+                    <div class="flex-1">
+                        <h1 class="text-black text-sm">Expired Year (YYYY) <span class="text-red-500">*</span></h1>
+                        <input class="w-full rounded-md border-none shadow-md" type="text" name="" id="">
+                    </div>
+                </div>
+                <div class="flex w-full gap-4">
+                    <div class="flex-1">
+                        <h1 class="text-black text-sm">Expired Month <span class="text-red-500">*</span></h1>
+                        <input class="w-full rounded-md border-none shadow-md" type="text" name="" id="">
+                    </div>
+                    <div class="flex-1">
+                        <h1 class="text-black text-sm">CVC</h1>
+                        <input class="w-full rounded-md border-none shadow-md" type="text" name="" id="">
+                    </div>
+                </div>
+                <div class="flex mt-6">
+                    <input type="checkbox" id="demoCheckbox" name="checkbox" value="1">&nbsp
+                    <p class="text-[#707070] text-sm">I have read and agree to the Terms and Conditions.</p>
+                </div>
+                <div class="flex justify-start items-start pt-8 w-4/6">
+                    <button class="bg-emerald-600 p-3 rounded text-white font-semibold" name="cars" id="cars">
+                        Proceed to Checkout
+                    </button>
+                </div>
+            </div>
+            <!-- end payment foam -->
+        </div>
+    </div>
+    <!-- end section one -->
+
+    <!-- start navigation -->
+    <div class="">
+        @include('layouts.footer')
+    </div>
+    <!-- end navigation -->
+
+</body>
+
+</html>
