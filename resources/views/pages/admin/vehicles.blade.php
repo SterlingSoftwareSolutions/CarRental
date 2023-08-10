b8996cd2cf29007d66624ca95e2a0cb333040c5a
Maneth Pathirana <manith.sampath@towcha.com.au>
2023-08-10 09:44:22 +0530

    Gave max Height

--- resources/views/pages/admin/vehicles.blade.php
+++ resources/views/pages/admin/vehicles.blade.php
@@ -3,11 +3,11 @@
     <x-admin-sidebar />
 
     <div class="sm:ml-64 p-6">
-        
+
         <!-- Top Grid  -->
         <div class="grid grid-cols-4 gap-10">
             <!-- Total Vehicles  -->
-            <div class="grid grid-cols-2 bg-white p-4 rounded-lg">
+            <div class="grid lg:grid-cols-2 bg-white p-4 rounded-lg">
                 <div class="">
                     <p class="text-gray-500">Vehicles</p>
                     <p class="text-gray-600 text-2xl font-bold">89,645</p>
@@ -67,6 +67,99 @@
             </div>
         </div>
 
+        <div class="mt-10">
+            <div class="relative overflow-auto shadow-md sm:rounded-lg bg-white max-h-96">
+                <div class="flex justify-end items-center">
+                    <div>
+                        <!-- Search bar  -->
+                        <form class="">
+                            <div class="relative">
+                                <div class="absolute inset-y-0 left-0 flex items-center pl-3">
+                                    <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                                    </svg>
+                                </div>
+                                <input type="search" id="default-search" class="border-none w-full p-4 pl-10 text-sm text-gray-500 bg-transparent focus:ring-0" placeholder="Search" required>
+                            </div>
+                        </form>
+                    </div>
+                    <div>
+                        <button class=" m-4 px-4 py-2 bg-main-green rounded-lg text-white"> + Add New Vehicle</button>
+                    </div>
+                </div>
+                <!-- Vehicle List  -->
+                <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                    <thead class="text-xs text-gray-700 uppercase dark:text-gray-400">
+                        <tr>
+                            <th scope="col" class="px-6 py-3">
+                                Vehicle
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                VIN
+                            </th>
+
+                            <th scope="col" class="px-6 py-3">
+                                Body Type
+                            </th>
+
+                            <th scope="col" class="px-6 py-3">
+                                Color
+                            </th>
+
+                            <th scope="col" class="px-6 py-3">
+                                <div class="flex items-center">
+                                    Price
+                                    <a href="#"><svg class="w-3 h-3 ml-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                                            <path d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z" />
+                                        </svg></a>
+                                </div>
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                <span class="sr-only">Edit</span>
+
+                            </th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        @for ($i = 0; $i < 100; $i++) <tr class="bg-white border-b">
+                            <th scope="row" class="px-6 py-4 font-medium text-gray-500 whitespace-nowrap">
+                                <div class="flex items-center">
+                                    <div class="me-3">
+                                        <img src="{{ URL('images/flg_logo11079.png')}}" class="rounded-full w-10 h-10" alt="Logo Image" id="dropdownDefaultButton" data-dropdown-toggle="dropdown">
+                                    </div>
+                                    <div>
+                                        <p>Nissan Sky-liner (Make + Model)</p>
+                                    </div>
+                                </div>
+                            </th>
+                            <td class="px-6 py-4">
+                                12345664645
+                            </td>
+                            <td class="px-6 py-4">
+                                Hatchback
+                            </td>
+                            <td class="px-6 py-4">
+                                White
+                            </td>
+                            <td class="px-6 py-4">
+                                $ 2999
+                            </td>
+                            <td class="px-6 py-4 text-right">
+                                <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                                /
+                                <a href="#" class="font-medium text-red-600 dark:text-red-500 hover:underline">Delete</a>
+                            </td>
+                            </tr>
+                            @endfor
+
+
+
+                    </tbody>
+                </table>
+            </div>
+
+        </div>
     </div>
 
 </x-app-layout>
