d534693b77e2ad06026952a42e6bb9fa4707b171
Maneth Pathirana <manith.sampath@towcha.com.au>
2023-08-13 16:19:16 +0530

    Routes Fixed

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,18 @@ Route::get('/', function () {
     return view('pages.home');
 })->name('home');
 
+Route::get('/about', function () {
+    return view('pages.about');
+})->name('about');
+
+Route::get('/carlist', function () {
+    return view('pages.carlist');
+})->name('carlist');
+
+Route::get('/contact', function () {
+    return view('pages.contact');
+})->name('contact');
+
 Route::get('/admin/dashboard', function () {
     return view('pages.admin.dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
@@ -30,9 +42,10 @@ Route::middleware('auth')->group(function () {
     Route::patch('/user/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/user/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     Route::get('/admin/users', [UsersController::class, 'show_all_users'])->name('users.all');
+    Route::get('/admin/user/{user_id}', [UsersController::class, 'edit_user'])->name('user.edit');
     Route::get('/admin/vehicles', [VehiclesController::class, 'show_all_vehicles'])->name('vehicles.all');
+    Route::get('/admin/vehicles/{vehicle_id}', [VehiclesController::class, 'edit_vehicle'])->name('vehicle.edit');
     Route::post('/admin/vehicles', [VehiclesController::class, 'store'])->name('vehicles.all');
 });
 
 require __DIR__ . '/auth.php';
-
