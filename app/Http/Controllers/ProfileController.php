c92a661a914bef443fab381bc7c0f029675f36d4
Naseef <AUNaseef@users.noreply.github.com>
2023-08-16 12:08:02 +0530

    updated profile update form

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
+use Illuminate\Validation\Rule;
 use Illuminate\View\View;
 
 class ProfileController extends Controller
@@ -24,17 +25,23 @@ class ProfileController extends Controller
     /**
      * Update the user's profile information.
      */
-    public function update(ProfileUpdateRequest $request): RedirectResponse
+    public function update(Request $request)
     {
-        $request->user()->fill($request->validated());
+        $user = $request->user();
 
-        if ($request->user()->isDirty('email')) {
-            $request->user()->email_verified_at = null;
-        }
-
-        $request->user()->save();
-
-        return Redirect::route('profile.edit')->with('status', 'profile-updated');
+        $values = $request->validate([
+            'first_name' => 'required',
+            'last_name' => 'required',
+            'email' => [
+                'required',
+                'string',
+                'email',
+                'max:255',
+                Rule::unique('users')->ignore($user->id),
+            ],
+        ]);
+        $user->update($values);
+        return back()->with('status', 'profile-updated');
     }
 
     /**
--- resources/views/pages/client/dashboard.blade.php
+++ resources/views/pages/client/dashboard.blade.php
@@ -1,97 +1,90 @@
 @php
-    $user = Auth::user();
-    $bookings = $user->bookings;
+$user = Auth::user();
+$bookings = $user->bookings;
 @endphp
 
-<x-app-layout>
-    <div class="p-6">
+@extends('layouts.main')
+@section('content')
+    <!-- BOOKING HISTORY -->
+    <h1 class="p-4 font-semibold text-lg text-[#707070] mt-7">Booking History</h1>
+    <div class="overflow-auto">
+        @if(count($bookings))
+        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400 ">
+            <thead class="text-xs text-gray-700 uppercase dark:text-gray-400">
+                <tr>
+                    <th scope="col" class="px-6 py-3">
+                        #
+                    </th>
 
-        <!-- BOOKING HISTORY -->
-        <div class="mt-24">
-            <h1 class="p-4 font-semibold text-lg text-[#707070] mt-7">Booking History</h1>
-            <div class="overflow-auto">
-                @if(count($bookings))
-                <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400 ">
-                    <thead class="text-xs text-gray-700 uppercase dark:text-gray-400">
-                        <tr>
-                            <th scope="col" class="px-6 py-3">
-                                #
-                            </th>
+                    <th scope="col" class="px-6 py-3">
+                        Pickup Location
+                    </th>
 
-                            <th scope="col" class="px-6 py-3">
-                                Pickup Location
-                            </th>
+                    <th scope="col" class="px-6 py-3">
+                        Pick up Date & Time
+                    </th>
 
-                            <th scope="col" class="px-6 py-3">
-                                Pick up Date & Time
-                            </th>
+                    <th scope="col" class="px-6 py-3">
+                        Drop Off Location
+                    </th>
 
-                            <th scope="col" class="px-6 py-3">
-                                Drop Off Location
-                            </th>
+                    <th scope="col" class="px-6 py-3">
+                        Drop off Date & Time
+                    </th>
 
-                            <th scope="col" class="px-6 py-3">
-                                Drop off Date & Time
-                            </th>
+                    <th scope="col" class="px-6 py-3">
+                        Amount
+                    </th>
 
-                            <th scope="col" class="px-6 py-3">
-                                Amount
-                            </th>
+                    <th scope="col" class="px-6 py-3 text-center">
+                        Status
+                    </th>
+                </tr>
+            </thead>
 
-                            <th scope="col" class="px-6 py-3 text-center">
-                                Status
-                            </th>
-                        </tr>
-                    </thead>
+            <tbody>
+                @foreach ($bookings as $booking)
+                <tr class="bg-white border-b">
+                    <th scope="row" class="px-6 py-4 font-medium text-gray-500 whitespace-nowrap">
+                        <div class="flex items-center">
+                            <div class="me-3">
+                                <img src="{{ Storage::url($booking->vehicle->images[0]->file_path ?? null) }}"
+                                    class="rounded-full w-10 h-10 object-cover" alt="Logo Image" id="dropdownDefaultButton"
+                                    data-dropdown-toggle="dropdown">
+                            </div>
+                            <div>
+                                <p>{{ $booking->vehicle['make']}} {{ $booking->vehicle['model']}} #{{
+                                    $booking->vehicle['vin']}}</p>
+                            </div>
+                        </div>
+                    </th>
 
-                    <tbody>
+                    <td class="px-6 py-4">
+                        {{ $booking['pickup']}}
+                    </td>
+                    <td class="px-6 py-4">
+                        {{ $booking['pickup_time']}}
+                    </td>
+                    <td class="px-6 py-4">
+                        {{ $booking['dropoff']}}
+                    </td>
+                    <td class="px-6 py-4">
+                        {{ $booking['dropoff_time']}}
+                    </td>
 
-                        @foreach ($bookings as $booking)
-                        <tr class="bg-white border-b">
-                            <th scope="row" class="px-6 py-4 font-medium text-gray-500 whitespace-nowrap">
-                                <div class="flex items-center">
-                                    <div class="me-3">
-                                        <img src="{{ Storage::url($booking->vehicle->images[0]->file_path ?? null) }}"
-                                            class="rounded-full w-10 h-10 object-cover" alt="Logo Image" id="dropdownDefaultButton"
-                                            data-dropdown-toggle="dropdown">
-                                    </div>
-                                    <div>
-                                        <p>{{ $booking->vehicle['make']}} {{ $booking->vehicle['model']}} #{{
-                                            $booking->vehicle['vin']}}</p>
-                                    </div>
-                                </div>
-                            </th>
+                    <td class="px-6 py-4">
+                        $ {{ $booking->amount()}}
+                    </td>
 
-                            <td class="px-6 py-4">
-                                {{ $booking['pickup']}}
-                            </td>
-                            <td class="px-6 py-4">
-                                {{ $booking['pickup_time']}}
-                            </td>
-                            <td class="px-6 py-4">
-                                {{ $booking['dropoff']}}
-                            </td>
-                            <td class="px-6 py-4">
-                                {{ $booking['dropoff_time']}}
-                            </td>
-
-                            <td class="px-6 py-4">
-                                $ {{ $booking->amount()}}
-                            </td>
-
-                            <td class="px-6 py-4 text-center">
-                                <span class="bg-gray-600 py-2 px-3 rounded-full text-white">{{$booking->status}}</span>
-                            </td>
-                        </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-                @else
-                <div class="px-6 py-4 font-medium text-gray-500 whitespace-nowrap">No Bookings Found</div>
-                @endif
-            </div>
-        </div>
-
+                    <td class="px-6 py-4 text-center">
+                        <span class="bg-gray-600 py-2 px-3 rounded-full text-white">{{$booking->status}}</span>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+        @else
+        <div class="px-6 py-4 font-medium text-gray-500 whitespace-nowrap">No Bookings Found</div>
+        @endif
     </div>
-
-</x-app-layout>
+@endsection
