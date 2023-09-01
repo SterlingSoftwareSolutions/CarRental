beafc874d1af8d8942a2d79826dfa40abe3647e1
Ravindu_Umayanga <74870496+RavinduISHA@users.noreply.github.com>
2023-09-01 13:17:48 +0530

    profile edit view fix

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,22 +1,22 @@
 @extends('layouts.main')
 @section('content')
     <div class="py-12">
-    <h1 class="p-4 font-semibold text-lg text-[#707070]">Edit Your Profile</h1>
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
+    <h1 class="p-4 font-semibold text-xl text-[#707070] text-center">Edit Your Profile</h1>
+        <div class="mx-auto space-y-6 max-w-7xl sm:px-6 lg:px-8">
+            <div class="p-4 mx-auto bg-white shadow max-w-[60%] sm:p-8 sm:rounded-lg">
+                <div class="max-w-xl mx-auto">
                     @include('profile.partials.update-profile-information-form')
                 </div>
             </div>
 
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
+            <div class="p-4 mx-auto bg-white shadow max-w-[60%] sm:p-8 sm:rounded-lg">
+                <div class="max-w-xl mx-auto">
                     @include('profile.partials.update-password-form')
                 </div>
             </div>
 
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
+            <div class="p-4 mx-auto bg-white shadow max-w-[60%] sm:p-8 sm:rounded-lg">
+                <div class="max-w-xl mx-auto">
                     @include('profile.partials.delete-user-form')
                 </div>
             </div>
