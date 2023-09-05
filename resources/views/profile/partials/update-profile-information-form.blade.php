2871672d06e2e4fae7d2ff07caf97e283ccc956d
Naseef <AUNaseef@users.noreply.github.com>
2023-09-05 17:02:18 +0530

    user profile page updated with additional fields

--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -39,16 +39,24 @@
             </script>
         </div>
 
-        <div>
-            <x-input-label for="first_name" :value="__('First Name')" />
-            <x-text-input id="first_name" name="first_name" type="text" class="mt-1 block w-full" :value="old('first_name', $user->first_name)" required autofocus autocomplete="name" />
-            <x-input-error class="mt-2" :messages="$errors->get('first_name')" />
+        <div class="flex gap-2">
+            <div class="w-1/2">
+                <x-input-label for="first_name" :value="__('First Name')" />
+                <x-text-input id="first_name" name="first_name" type="text" class="mt-1 block w-full" :value="old('first_name', $user->first_name)" required autofocus autocomplete="name" />
+                <x-input-error class="mt-2" :messages="$errors->get('first_name')" />
+            </div>
+
+            <div class="w-1/2">
+                <x-input-label for="last_name" :value="__('Last Name')" />
+                <x-text-input id="last_name" name="last_name" type="text" class="mt-1 block w-full" :value="old('last_name', $user->last_name)" required autofocus autocomplete="name" />
+                <x-input-error class="mt-2" :messages="$errors->get('last_name')" />
+            </div>
         </div>
 
         <div>
-            <x-input-label for="last_name" :value="__('Last Name')" />
-            <x-text-input id="last_name" name="last_name" type="text" class="mt-1 block w-full" :value="old('last_name', $user->last_name)" required autofocus autocomplete="name" />
-            <x-input-error class="mt-2" :messages="$errors->get('last_name')" />
+            <x-input-label for="mobile" :value="__('Phone Number')" />
+            <x-text-input id="mobile" name="mobile" type="text" class="mt-1 block w-full" :value="old('mobile', $user->mobile)" required autofocus autocomplete="name" />
+            <x-input-error class="mt-2" :messages="$errors->get('mobile')" />
         </div>
 
         <div>
@@ -73,6 +81,32 @@
                     @endif
                 </div>
             @endif
+        </div>
+
+        <div>
+            <x-input-label for="Address_1" :value="__('Address Line 1')" />
+            <x-text-input id="Address_1" name="Address_1" type="text" class="mt-1 block w-full" :value="old('Address_1', $user->Address_1)" required autofocus autocomplete="name" />
+            <x-input-error class="mt-2" :messages="$errors->get('Address_1')" />
+        </div>
+
+        <div>
+            <x-input-label for="Address_2" :value="__('Address Line 2')" />
+            <x-text-input id="Address_2" name="Address_2" type="text" class="mt-1 block w-full" :value="old('Address_2', $user->Address_2)" required autofocus autocomplete="name" />
+            <x-input-error class="mt-2" :messages="$errors->get('Address_2')" />
+        </div>
+
+        <div class="flex gap-2">
+            <div class="w-1/2">
+                <x-input-label for="city" :value="__('City')" />
+                <x-text-input id="city" name="city" type="text" class="mt-1 block w-full" :value="old('city', $user->city)" required autofocus autocomplete="name" />
+                <x-input-error class="mt-2" :messages="$errors->get('city')" />
+            </div>
+
+            <div class="w-1/2">
+                <x-input-label for="zip" :value="__('ZIP')" />
+                <x-text-input id="zip" name="zip" type="text" class="mt-1 block w-full" :value="old('zip', $user->zip)" required autofocus autocomplete="name" />
+                <x-input-error class="mt-2" :messages="$errors->get('zip')" />
+            </div>
         </div>
 
         <div class="flex items-center gap-4">
