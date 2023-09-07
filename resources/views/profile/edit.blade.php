224ac18f71c64fd4167b70a9c1f87c4429a849aa
Ravindu_Umayanga <74870496+RavinduISHA@users.noreply.github.com>
2023-09-07 14:41:09 +0530

    center edit text

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     <div class="py-12 max-w-[1000px] mx-auto">
         <div class="mx-auto space-y-6 sm:px-6 lg:px-8">
-            <h1 class="font-semibold text-lg text-[#707070]">Edit Your Profile</h1>
+            <h1 class="font-semibold text-lg text-[#707070] text-center">Edit Your Profile</h1>
             <div class="p-4 bg-white shadow sm:p-8 sm:rounded-lg">
                 <div class="w-full sm:hidden md:block">
                     @include('profile.partials.update-profile-information-form')
