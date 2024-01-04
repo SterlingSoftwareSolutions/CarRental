1d2d574daa387db46501c3b85ba8644ed00468f3
Naseef <AUNaseef@users.noreply.github.com>
2024-01-04 18:03:51 +0530

    Fix accordion indicator glitch

--- resources/views/components/accordion.blade.php
+++ resources/views/components/accordion.blade.php
@@ -6,8 +6,7 @@
 <div x-data="{ open: {{$open}} }" class="rounded-md overflow-clip mt-4" >
     <div class="w-full p-2 cursor-pointer flex justify-between items-center bg-slate-200 bg-opacity-50 hover:bg-opacity-75 transition-colors" x-on:click="open = !open">                            
         <h1 class="w-full font-light text-slate-800">{{$title}}</h1>
-        <div class="fa fa-angle-down p-2 bg-white rounded-full text-slate-600" x-show="!open"></div>
-        <div class="fa fa-angle-up p-2 bg-white rounded-full text-slate-600" x-show="open"></div>
+        <div class="p-2 bg-white fa fa-angle-down fa-angle-up rounded-full text-slate-600" :class="{ 'fa-angle-up ': open }"></div>
     </div>
     <div class="flex flex-wrap bg-slate-50 pb-1" x-show="open">
         {{$slot}}
